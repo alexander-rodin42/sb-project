b821500414337cf0be873993216f9200ff19cb5b
Project177117 <Project177117@gmail.com>
2021-11-10 11:17:21 +0500

    3-IP-address-validation v1.0

--- 11-functions/3-IP-address-validation/main.cpp
+++ 11-functions/3-IP-address-validation/main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <string>
+
+bool CheckNumber (std::string number) {
+    if (number.empty() || number.length() > 3 || number.length() > 1 && number[0] == '0') {
+        return false;
+    } else {
+        int currentNumber = 0;
+        int numberMultiplier = 1;
+
+        for (char i: number) {
+            if (i < '0' || i > '9') {
+                return false;
+            }
+            currentNumber = currentNumber * numberMultiplier + (int)(i - '0');
+            numberMultiplier = 10;
+        }
+        return currentNumber >= 0 && currentNumber < 256;
+    }
+}
+
+std::string CheckIpAddress (const std::string& ipAddress) {
+
+    int pointsAmount = 0;
+    std::string currentNumber;
+
+    for (char i : ipAddress) {
+        if (i == '.') {
+            if (!CheckNumber( currentNumber)) {
+                return "No";
+            }
+            pointsAmount++;
+            currentNumber = "";
+        } else {
+            currentNumber += i;
+        }
+    }
+    if (!CheckNumber( currentNumber)) {
+        return "No";
+    }
+    return pointsAmount == 3 ? "Yes" : "No";
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string userIpAddress;
+
+    std::cout << "Enter the IP address: ";
+    std::cin >> userIpAddress;
+
+    std::cout << CheckIpAddress( userIpAddress) << std::endl;
+
     return 0;
 }
