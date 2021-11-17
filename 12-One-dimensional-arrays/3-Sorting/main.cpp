8f3cf992faab0f25bed99580dd8fafdb98c65407
Project177117 <Project177117@gmail.com>
2021-11-17 16:08:58 +0500

    12-3-Sorting v1.0

--- 12-One-dimensional-arrays/3-Sorting/main.cpp
+++ 12-One-dimensional-arrays/3-Sorting/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
 
 int main() {
+    float userNumbers[15];
+
+    std::cout << "Enter 15 floating point numbers:" << std::endl;
+    for (int i = 0; i < 15; i++) {
+        std::cout << i << " - ";
+        std::cin >> userNumbers[i];
+    }
+
+    float minimumNumber;
+    int minimumIndex;
+
+    for (int i = 0; i < 14; i++) {
+        minimumNumber = userNumbers[i];
+        minimumIndex = i;
+        for (int j = i + 1; j < 15; j++) {
+            if (userNumbers[j] < minimumNumber) {
+                minimumNumber = userNumbers[j];
+                minimumIndex = j;
+            }
+        }
+
+        for (int j = minimumIndex; j > i; j--) {
+            userNumbers[j] = userNumbers[j - 1];
+        }
+        userNumbers[i] = minimumNumber;
+    }
+
+    for (float i : userNumbers) {
+        std::cout << i << " ";
+    }
+
     return 0;
 }
