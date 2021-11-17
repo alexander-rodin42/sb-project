1bd41872ebfdb9e3dcb65ed694a1bae62397353d
Project177117 <Project177117@gmail.com>
2021-11-17 14:36:22 +0500

    2-Row-of-numbers v1.0

--- 12-One-dimensional-arrays/2-Row-of-numbers/main.cpp
+++ 12-One-dimensional-arrays/2-Row-of-numbers/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main() {
+    int numbers[15] = {114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118};
+
+    for (int i = 0; i < 14; i++) {
+        for (int j = i + 1; j < 15; j++) {
+            if (numbers[i] == numbers[j]) {
+                std::cout << numbers[i] << std::endl;
+                //std::cout << i << " - " << j << std::endl;
+            }
+        }
+    }
     return 0;
 }
