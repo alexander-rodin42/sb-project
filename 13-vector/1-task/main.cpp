a7f023a4a8fcc462d0999090e675663c20580d54
Project177117 <Project177117@gmail.com>
2021-11-29 14:48:27 +0500

    add main.cpp v1.0 in 13-1-task

---
+++ 13-vector/1-task/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> RemoveNumbers (const std::vector<int>& numbers, int deleteNumbers) {
+    std::vector<int> result;
+
+    for (int number : numbers) {
+        if (number != deleteNumbers) {
+            result.push_back(number);
+        }
+    }
+    return result;
+}
+
+void printVector (const std::vector<int>& vector) {
+    for (int number : vector) {
+        std::cout << number << " ";
+    }
+}
+
+int main() {
+    int amountNumbers;
+    int deleteNumber;
+
+    do {
+        std::cout << "Enter digits of number:";
+        std::cin >> amountNumbers;
+    } while (amountNumbers < 0);
+
+    std::vector<int> numbers( amountNumbers);
+
+    for (int i = 0; i < amountNumbers; i++) {
+        std::cout << "Enter number #" << i << ": ";
+        std::cin >> numbers[i];
+    }
+
+    std::cout << "Enter the number to remove:";
+    std::cin >> deleteNumber;
+
+    numbers = RemoveNumbers( numbers, deleteNumber);
+
+    if (numbers.empty()) {
+        std::cout << "Vector is empty." << std::endl;
+    } else {
+        printVector(numbers);
+    }
+
+    return 0;
+}
