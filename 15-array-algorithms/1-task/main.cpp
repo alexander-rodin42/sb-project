0bf6cd4a83b7e3be3c706ec0cc78a37ad83ab849
Project177117 <Project177117@gmail.com>
2021-12-28 17:09:36 +0500

    Kadane's algorithm

--- 15-array-algorithms/1-task/main.cpp
+++ 15-array-algorithms/1-task/main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <vector>
 
+//Kadane's algorithm
+
 int main() {
     std::vector<int> sequenceNumbers = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int startIndex, endIndex, sum;
-    int maxSum = sequenceNumbers[0];
+
+    int startIndex = 0,
+        endIndex = 0,
+        minusPos = -1,
+        sum = 0,
+        maxSum = sequenceNumbers[0];
 
     for (int i = 0; i < sequenceNumbers.size(); i++) {
-        sum = sequenceNumbers[i];
-        for (int j = i + 1; j < sequenceNumbers.size(); j++) {
-            sum += sequenceNumbers[j];
+        sum += sequenceNumbers[i];
 
-            if (sum > maxSum) {
-                maxSum = sum;
-                startIndex = i;
-                endIndex = j;
-            }
+        if (sum > maxSum) {
+            maxSum = sum;
+            startIndex = minusPos + 1;
+            endIndex = i;
         }
+
+        if (sum < 0) {
+            sum = 0;
+            minusPos = i;
+        }
+
     }
     std::cout << startIndex << " " << endIndex << std::endl;
 
