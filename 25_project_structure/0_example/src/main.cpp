6aa75e3ccaedefbb4c658f61a01743ce13c41b7a
Project177117 <Project177117@gmail.com>
2022-03-25 08:27:25 +0500

    add CMake.txt

---
+++ 25_project_structure/0_example/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "my-math.h"
+#include "my-math.h" //Add "#pragma once" in header file
+
+int main() {
+    int temp;
+    std::cout << circumference( 3.5) << std::endl;
+    std::cin >> temp;
+    return 0;
+}
